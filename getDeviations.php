fdf3a9ec5a62d8e18904856ffadcda2d1602ef70
jamesl1001 <jamesl1001@msn.com>
2019-10-31 21:30:26 +0000

    Fix Node no longer exists errors for PHP7

--- getDeviations.php
+++ getDeviations.php
@@ -21,14 +21,14 @@ function getDeviations($url, $limit = null, $start = 0) {
             "url"           => $item->link,
             "date"          => $item->pubDate,
             "desc"          => $item->children('media', true)->description,
-            "thumbS"        => $item->children('media', true)->thumbnail->{0}->attributes()->url,
-            "thumbL"        => $item->children('media', true)->thumbnail->{1}->attributes()->url,
+            "thumbS"        => $item->children('media', true)->thumbnail[0]->attributes()->url,
+            "thumbL"        => $item->children('media', true)->thumbnail[1]->attributes()->url,
             "image"         => $item->children('media', true)->content->attributes()->url,
             "rating"        => $item->children('media', true)->rating,
             "categoryUrl"   => $item->children('media', true)->category,
             "category"      => $item->children('media', true)->category->attributes()->label,
-            "deviantName"   => $item->children('media', true)->credit->{0},
-            "deviantAvatar" => $item->children('media', true)->credit->{1},
+            "deviantName"   => $item->children('media', true)->credit[0],
+            "deviantAvatar" => $item->children('media', true)->credit[1],
             "deviantUrl"    => $item->children('media', true)->copyright->attributes()->url,
             "copyright"     => $item->children('media', true)->copyright
         );
